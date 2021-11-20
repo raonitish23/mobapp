d94191bb21c8760338cd247d3b6c07f027fbcb88
Raonitish <nyadav2121@gmail.com>
2021-11-20 11:02:24 +0530

    dev dependency

--- configs/db.js
+++ configs/db.js
@@ -13,11 +13,11 @@ const options = {
   connectTimeoutMS: 10000,
 };
 
-//let url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+let url = `mongodb+srv://admin:admin123@cluster0.h0mb8.mongodb.net/mobapp?retryWrites=true&w=majority`;
 
 
 module.exports = {
   'secret': '',
-  'database': 'mongodb://127.0.0.1:27017/learning',
+  'database': url,
   'options': options
 };
